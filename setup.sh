d5d005ec4331a36b4b965e177f7faa6eb4a8bb2c
jhon parra <jhonparra939@gmail.com>
2022-05-11 11:51:11 -0500

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 apt-get update -y && apt-get install -y --no-install-recommends build-essential gcc libsndfile1
 apt-get install screen -y
-apt install nvtop
 pip install soundfile
 pip install datasets --upgrade
 pip install jiwer
